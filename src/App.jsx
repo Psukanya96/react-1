855a9951c8b2fef8d606324699e768f3a994264e
Sukanya <sukanya@techversantinfo.com>
2023-07-30 13:28:57 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import React from "react";
-import DataView from "./components/DataView/DataView"; // File - assumes .js extension if not provided.
-import Navbar from "./components/Navbar"; // Directory - assumes index.js if none listed.
+import DataView from "./components/DataView/DataView"; 
+import Navbar from "./components/Navbar";
 import "./App.css";
-
-// We're using an es6 arrow function, but this could also be written:
-// function App() {}
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
       <h1 className="header">Earn More, Pay Less? </h1>
-      <h2 className="subheader">
-        Average Housing Costs and Incomes by NYC Borough
-      </h2>
-      <DataView />
+      <h2 className="subheader">Average Housing Costs and Incomes by NYC Borough</h2>
+
+      {/* DataView component for Staten Island */}
+      <DataView borough="Staten Island" renterCost={14292} ownerCost={28752} renterIncome={37882} ownerIncome={94177} />
+
+      {/* DataView component for Queens */}
+      <DataView borough="Queens" renterCost={13560} ownerCost={26052} renterIncome={37875} ownerIncome={83397} />
+
+      {/* DataView component for Brooklyn */}
+      <DataView borough="Brooklyn" renterCost={16092} ownerCost={27684} renterIncome={45166} ownerIncome={101997} />
+
+      {/* DataView component for Manhattan */}
+      <DataView borough="Manhattan" renterCost={23472} ownerCost={49104} renterIncome={55354} ownerIncome={132025} />
+
+      {/* DataView component for Bronx */}
+      <DataView borough="Bronx" renterCost={12300} ownerCost={24540} renterIncome={29280} ownerIncome={59974} />
     </div>
   );
 };
--- src/components/DataView/DataView.jsx
+++ src/components/DataView/DataView.jsx
@@ -2,15 +2,20 @@ import React from "react";
 import "./DataView.css";
 
 const DataView = (props) => {
+  const { borough, renterCost, ownerCost, renterIncome, ownerIncome } = props;
+
+  //  Cost-To-Income ratio for renters and owners
+  const renterRatio = (renterCost / renterIncome)*100;
+  const ownerRatio = (ownerCost / ownerIncome)*100;
+
   return (
     <div className="view">
       {/* left side of card */}
       <div className="view-column">
         <div className="borough">
-          <h1 className="borough-text">{props.borough}</h1>
+          <h1 className="borough-text">{borough}</h1>
         </div>
       </div>
-      {/* right side of card */}
       <div className="view-column">
         <table className="data-view">
           <tr className="data-view-row">
@@ -23,15 +28,38 @@ const DataView = (props) => {
           </tr>
           <tr className="data-view-row">
             <td className="text">
-              <h2 className="text-data">{props.renterCost}</h2>
+              <h2 className="text-data">{renterCost}</h2>
               <h3 className="text-label">Annual Housing Cost</h3>
             </td>
             <td className="text">
-              <h2 className="text-data">{}</h2>
+              <h2 className="text-data">{ownerCost}</h2>
               <h3 className="text-label">Annual Housing Cost</h3>
             </td>
           </tr>
-        </table>
+          <tr className="data-view-row">
+            <td className="text">
+              <h2 className="text-data">{renterIncome}</h2>
+              <h3 className="text-label">Annual Income</h3>
+            </td>
+            <td className="text">
+              <h2 className="text-data">{ownerIncome}</h2>
+              <h3 className="text-label">Annual Income</h3>
+            </td>
+          </tr>
+          <tr className="data-view-row">
+            <td className="text">
+              <h2 className="text-data"> {renterRatio}%</h2>
+              <h3 className="text-label">Cost-To-Income Ratio</h3>
+            </td>
+            <td className="text">
+              <h2 className="text-data">{ownerRatio}%</h2>
+              <h3 className="text-label">Cost-To-Income Ratio</h3>
+            </td>
+          </tr>
+        </table> 
+        <p className="cost-of-living">
+          Cost of living vastly differs for renters and buyers.
+        </p>
       </div>
     </div>
   );
